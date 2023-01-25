8f499724c0d16750c462fac1f289de8cdc3b322f
Jossl123 <jossl1.gau@gmail.com>
2023-01-25 14:48:50 +0100

    update stricker

--- plankton/main.py
+++ plankton/main.py
@@ -24,39 +24,55 @@ def rotateVector(v, theta):
 
 class ExampleManager(Manager):
     def step(self):
-        self.setSideValue()
-        # self.go_to(self.allies1, x=0.0, y=0.0, orientation=0.0)
-        # self.control(self.allies2, forward_velocity=1.0, left_velocity=0.0, angular_velocity=0.0)
-        # self.client.commands.append(Command(id=0, forward_velocity=1.0, angular_velocity=0.0))
-        # print(self.ball)
-        toTarget = self.goalTarget() - self.ball
-        toTarget = -(normalize(toTarget)*0.1)
+        nearestId = 0
+        nearestDist = 100
+        for i in range(6):
+            if not self.allie(i):continue
+            self.initEnv(i)
+
+            #get nearset robot to ball
+            d = self.distToBall(i)
+            if d < nearestDist:
+                nearestId = i
+                nearestDist = d
+
+            #reset velo and angle
+            self.control(self.allie(i), forward_velocity=0.0, left_velocity=0.0, angular_velocity=0.0)
+        id = nearestId
+
+        toTarget = self.goalTarget(id) - self.ball
+        toTarget = -(normalize(toTarget)*0.1) #0.1 = dist to ball
         shootingPos = toTarget + self.ball
+        
+        kick = self.wantToKick(id)
+        self.go_to(self.allie(id), x=shootingPos[0], y=shootingPos[1], orientation=self.angleTo(self.allie(id).position, self.ball), kick=kick, power=10, dribble=0)
 
-        kick = self.wantToKick()
+    def allie(self, id):
+        return self.robots['allies'][id]
 
-        self.go_to(self.allies1, x=shootingPos[0], y=shootingPos[1], orientation=self.angleTo(self.allies1.position, self.ball), kick=kick, power=10, dribble=1)
+    def wantToKick(self, id):
+        targetDir = rotateVector(np.array([1, 0]), self.allie(id).orientation)
 
-    def wantToKick(self):
-        targetDir = rotateVector(np.array([1, 0]), self.allies1.orientation)
+        goalX = np.array([-self.allie(id).side * self.field['length']/2, self.field['goal_width']/2.4])
+        goalY = np.array([-self.allie(id).side * self.field['length']/2, -self.field['goal_width']/2.4])
 
-        goalX = np.array([-self.allies1.side * self.field['length']/2, self.field['goal_width']/2])
-        goalY = np.array([-self.allies1.side * self.field['length']/2, -self.field['goal_width']/2])
+        inGoal = intersect(self.allie(id).position, self.allie(id).position + targetDir * 10, goalX, goalY)
+        print(inGoal, self.distToBall(id) < 0.11)
+        return KICK.STRAIGHT_KICK if (self.distToBall(id) < 0.11 and inGoal) else KICK.NO_KICK
 
-        inGoal = intersect(self.allies1.position, self.allies1.position + targetDir * 10, goalX, goalY)
-        return KICK.STRAIGHT_KICK if (self.distToBall() < 0.11 and inGoal) else KICK.NO_KICK
+    def distToBall(self, id):
+        return np.linalg.norm(self.allie(id).position - self.ball)
 
-    def distToBall(self):
-        return np.linalg.norm(self.allies1.position - self.ball)
-
-    def goalTarget(self):
-        x = -self.allies1.side * self.field['length']/2
+    def goalTarget(self, id):
+        x = -self.allie(id).side * self.field['length']/2
         y = 0
+        print(np.array([x,y]))
         return np.array([x,y])
 
-    def setSideValue(self):
-        if not hasattr(self.allies1, "side"): 
-            self.allies1.side = 1 if self.allies1.position[0] > 0 else -1
+    def initEnv(self, id):
+        if not hasattr(self.allie(id), "side"):
+            self.allie(id).side = 1 if self.allie(id).position[0] > 0 else -1
+            self.allie(id).role = ''
     
     def angleTo(self, p1, p2):
         v = p1 - p2
