5fa5254bbc09d7bcff8fcd94559f5fb99bd3795a
MENIER Thomas Wanchaï <thomas.menier@etu.u-bordeaux.fr>
2023-02-14 18:07:42 +0100

    feat: field can guess the goal keeper of each team

--- plankton/field_observer.py
+++ plankton/field_observer.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import utils
+from plankton.goalkeeper import determine_goal_area
 from plankton_client import Robot
 
 
@@ -11,6 +12,7 @@ class FieldObserver:
         self.__ball_traj_m_p: tuple[float, float] = (0, 0)
         self.__ball_moving: bool = False
         self.__closest_rob_to_ball: Robot = None
+        self.__guessed_goalkeepers: dict[str, Robot] = None
 
     def __update_ball_history(self, ball: tuple[float, float]):
         if len(self.__ball_history) >= 40:
@@ -33,16 +35,26 @@ class FieldObserver:
     def __update_closest_robot_to_ball(self, ball: tuple[float, float], robots: list[Robot]):
         self.__closest_rob_to_ball = utils.closest_to_target(robots, target=ball)
 
+    def __guess_goal_keepers(self, robots_dict: dict[str, list[Robot]], field: dict, is_blue_x_positive: bool):
+        ally_gk = utils.closest_to_target(robots_dict["allies"], determine_goal_area(field, is_blue_x_positive))
+        enemy_gk = utils.closest_to_target(robots_dict["enemies"], determine_goal_area(field, not is_blue_x_positive))
+        self.__guessed_goalkeepers = {
+            "allies": ally_gk,
+            "enemies": enemy_gk
+        }
+
     def get_data(self):
         return {
             "ball_traj__m_p": self.__ball_traj_m_p,
             "ball_history": self.__ball_history,
             "ball_moving": self.__ball_moving,
             "first_rob_near_ball": self.__closest_rob_to_ball,
+            "goalkeepers": self.__guessed_goalkeepers,
         }
 
-    def step(self, ball: tuple[float, float], robots: list[Robot]):
+    def step(self, ball: tuple[float, float], robots_dict: dict[str, list[Robot]], field: dict, is_blue_x_positive: bool):
         self.__update_ball_history(ball)
         self.__check_ball_moving()
         self.__compute_ball_traj()
-        self.__update_closest_robot_to_ball(ball, robots)
+        self.__update_closest_robot_to_ball(ball, robots_dict["allies"] + robots_dict["enemies"])  # concat go brrr
+        self.__guess_goal_keepers(robots_dict, field, is_blue_x_positive)
